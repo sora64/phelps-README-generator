c921b2b4fcb9c2b1fd197baa6e4d9f6b150fc7cb
Alan Phelps <phelpsa64@gmail.com>
2022-01-29 11:40:22 -0500

    can write readme.md with a changing title

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const path = require('path');
-const markdown = require('markdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const path = require('path');
+// const { FILE } = require('dns');
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    // TODO: Create an array of questions for user input
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is name of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project name!');
+                    return false;
+                }
+            }
+        }
+    ]);
+};
+
+// TODO: Create a function to write README file
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+init()
+    .then(readmeData => {
+        return generateMarkdown(readmeData);
+    })
+    .then(readmeMarkdown => {
+        return writeToFile(readmeMarkdown);
+    });
+
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = license => {
+  if (!license) {
+    return '';
+  }
+
+  return `
+    ${license}
+  `;
+};
+
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,10 +21,21 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
+module.exports = generateMarkdown => {
+  const data = generateMarkdown;
 
-module.exports = generateMarkdown;
+  console.log(data);
+
+  return `
+  # ${data.title}
+
+  `;
+};
+
+// function generateMarkdown(data) {
+//   return `
+//   # ${data.title}
+
+//   `;
+// };
